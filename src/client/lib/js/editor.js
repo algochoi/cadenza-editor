8db7f5de7200adfc96dbc553d958bdd5d432b6c1
algochoi <86622919+algochoi@users.noreply.github.com>
2022-04-06 14:28:23 -0400

    Output to console

--- src/client/lib/js/editor.js
+++ src/client/lib/js/editor.js
@@ -1,5 +1,7 @@
 const compileCodeBtn = document.querySelector('.editor__run');
 const deployCodeBtn = document.querySelector('.editor__deploy');
+const consoleLogList = document.querySelector('.editor__console-logs');
+
 
 const editor = ace.edit("editor");
 const defaultCode = `from pyteal import *
@@ -7,6 +9,8 @@ const defaultCode = `from pyteal import *
 def approval():
     return Approve()
 `;
+const consoleMessages = [];
+
 const editorLib = {
     init() {
         editor.session.setMode("ace/mode/python");
@@ -16,6 +20,17 @@ const editorLib = {
         });
 
         editor.setValue(defaultCode)
+    },
+
+    outputConsole() {
+        const newLog = document.createElement('li');
+        const newLogText = document.createElement('pre');
+
+        newLogText.className = "log log--string";
+        newLogText.textContent = `> ${consoleMessages[0].item}`;
+
+        newLog.appendChild(newLogText);
+        consoleLogList.appendChild(newLog);
     }
 };
 
@@ -24,6 +39,8 @@ compileCodeBtn.addEventListener('click', () => {
     const userCode = editor.getValue();
 
     console.log(userCode);
+
+    editorLib.outputConsole();
 });
 
 
